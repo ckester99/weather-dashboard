b3d741c5d1e762b72c72fa205a8e6d0f46c024ab
Christian L. Kester <christiankester77@gmail.com>
2022-11-15 13:49:35 -0500

    added search history button functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 var citySearchFrm = $("form");
 var currentDayDisp = $("#current-day-display");
 var forecastCards = $(".card");
+var historyList = $("#search-history");
 const apiKey = "934e2a893482bf1e8b8f916e9379281e";
 
 citySearchFrm.children("button").click(handleFormSubmit);
@@ -20,15 +21,14 @@ async function handleFormSubmit(event) {
         if (geocodes.length === 0) {
             alert("Please enter a valid city");
         } else {
-            searchWeather(geocodes[0]["name"], geocodes[0]["lat"], geocodes[0]["lon"]);
+            displayWeather(geocodes[0]["name"], geocodes[0]["lat"], geocodes[0]["lon"]);
+            addCity(geocodes[0]["name"], geocodes[0]["lat"], geocodes[0]["lon"]);
             textInput.val("");
         }
     }
 }
 
-async function searchWeather(city, lat, lon) {
-    console.log(lat);
-    console.log(lon);
+async function displayWeather(city, lat, lon) {
     var requestUrl = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=" + apiKey;
 
     var curWeather = await fetch(requestUrl).then(function (response) {
@@ -39,7 +39,6 @@ async function searchWeather(city, lat, lon) {
     dayIcon.attr("src", "http://openweathermap.org/img/wn/" + curWeather.weather[0].icon + "@2x.png");
     dayIcon.attr("alt", "Weather Icon");
     dayIcon.attr("width", "70px");
-    console.log(currentDayDisp.children("p"));
     $(currentDayDisp.children("p")[0]).text("Temp: " + curWeather.main.temp + " °F");
     $(currentDayDisp.children("p")[1]).text("Wind: " + curWeather.wind.speed + " MPH");
     $(currentDayDisp.children("p")[2]).text("Humidity: " + curWeather.main.humidity + " %");
@@ -49,8 +48,6 @@ async function searchWeather(city, lat, lon) {
         return response.json();
     });
     forecast = forecast.list;
-    console.log(forecast);
-    console.log(forecast.length);
     for (var i = 0; i < forecastCards.length; i++) {
         $(forecastCards[i])
             .children("h3")
@@ -63,6 +60,18 @@ async function searchWeather(city, lat, lon) {
         $($(forecastCards[i]).children("p")[1]).text("Wind: " + forecast[i * 8 + 7].wind.speed + " MPH");
         $($(forecastCards[i]).children("p")[2]).text("Humidity: " + forecast[i * 8 + 7].main.humidity + " %");
     }
+}
 
-    console.log(dayjs.unix(curWeather.dt));
+function addCity(city, lat, lon) {
+    var historyBtn = $("<button class='mb-3 py-1 col-9 position-relative translate-middle-x start-50 rounded-5 text-light' >").appendTo(historyList);
+    historyBtn.text(city);
+    historyBtn.attr("city", city);
+    historyBtn.attr("lat", lat);
+    historyBtn.attr("lon", lon);
+    historyBtn.click(showHistoryData);
+}
+
+function showHistoryData(event) {
+    var buttonClicked = $(event.currentTarget);
+    displayWeather(buttonClicked.attr("city"), buttonClicked.attr("lat"), buttonClicked.attr("lon"));
 }
